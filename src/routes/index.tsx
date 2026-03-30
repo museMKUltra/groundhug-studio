adaffd196c29b2fc8f09f5c2e37a7d97a6533833
zebra <roller@rexlite.com.tw>
2026-03-30 15:27:03 +0800

    feat: add `GuestRoute` component to handle redirection for authenticated users

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,16 @@ import {Route, Routes} from "react-router-dom";
 import LoginPage from "@/pages/LoginPage";
 import AttendancePage from "@/pages/AttendancePage";
 import ProtectedRoute from "@/routes/ProtectedRoute";
+import GuestRoute from "@/routes/GuestRoute.tsx";
 
 export default function AppRoutes() {
     return (
         <Routes>
-            <Route path="/login" element={<LoginPage/>}/>
+            <Route path="/login" element={
+                <GuestRoute>
+                    <LoginPage/>
+                </GuestRoute>
+            }/>
 
             <Route
                 path="/attendance"
