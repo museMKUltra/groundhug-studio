3adc066b6da8fc39dd3e032f1bc9296c99f42583
zebra <roller@rexlite.com.tw>
2026-03-31 14:35:41 +0800

    feat: add MUI CssBaseline for global styles setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import {BrowserRouter} from "react-router-dom";
+import CssBaseline from "@mui/material/CssBaseline";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <BrowserRouter>
-            <App/>
+            <>
+                <CssBaseline/>
+                <App/>
+            </>
         </BrowserRouter>
     </React.StrictMode>
 );
