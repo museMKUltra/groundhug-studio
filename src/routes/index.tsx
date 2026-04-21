92b54ab9e587a7db187251cd977ee6596ba1938f
zebra <roller@rexlite.com.tw>
2026-04-21 11:20:39 +0800

    feat(routes): add redirect from `/` to `/login`
    - Introduced `<Navigate>` in `AppRoutes` to ensure `/` redirects to the login page by default.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import MainLayout from "@/layouts/MainLayout.tsx";
 import GuardPipeline from "@/routes/GuardPipeline.tsx";
 import {routes} from "./config";
@@ -6,6 +6,8 @@ import {routes} from "./config";
 export default function AppRoutes() {
     return (
         <Routes>
+            <Route path="/" element={<Navigate to="/login" replace/>}/>
+
             {routes.map((route) => (
                 <Route
                     key={route.path}
