c2f57c55e636760275a6771936a46df1cdca178e
zebra <roller@rexlite.com.tw>
2026-04-27 14:26:08 +0800

    refactor(auth): merge `auth.context` into `useAuthContext` for cleanup and simplification

--- src/features/auth/AuthProvider.tsx
+++ src/features/auth/AuthProvider.tsx
@@ -1,6 +1,6 @@
 import {useState} from "react";
 import type {User} from "./types";
-import {AuthContext} from "./auth.context";
+import {AuthContext} from "./useAuthContext";
 import {tokenStorage} from "@/features/auth/tokenStorage.ts";
 import {jwtDecode} from "jwt-decode";
 
--- src/features/auth/useAuthContext.ts
+++ src/features/auth/useAuthContext.ts
@@ -1,5 +1,16 @@
 import {useContext} from "react";
-import {AuthContext} from "./auth.context";
+import {createContext} from "react";
+import type {User} from "./types";
+
+export type AuthContextType = {
+    user: User | null;
+    hourlyRate: number;
+    setUser: (user: User | null) => void;
+    updateUser: (updates: Partial<User>) => void;
+    setHourlyRate: (rate: number) => void;
+};
+
+export const AuthContext = createContext<AuthContextType | null>(null);
 
 export const useAuthContext = () => {
     const ctx = useContext(AuthContext);
